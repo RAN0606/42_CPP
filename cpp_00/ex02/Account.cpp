afa8113c4881665b6c48e00e721f8a0351524e83
Ran LIU <rliu@e1r4p10.clusters.42paris.fr>
2022-09-21 19:25:50 +0200

    need to finish Account.cpp

---
+++ cpp_00/ex02/Account.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rliu <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 16:27:46 by rliu              #+#    #+#             */
+/*   Updated: 2022/09/21 16:27:52 by rliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+ 
+Account::Account(int  initial_deposit) : {
+
+}
