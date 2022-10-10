2e5cbff66a5ebab9874f228b727ef28cc5efbc22
Ran LIU <rliu@e1r5p8.clusters.42paris.fr>
2022-10-10 21:06:12 +0200

    x

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -6,20 +6,29 @@
 /*   By: rliu <rliu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:02:39 by rliu              #+#    #+#             */
-/*   Updated: 2022/10/10 17:14:23 by rliu             ###   ########.fr       */
+/*   Updated: 2022/10/10 21:05:38 by rliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed(void):_nbFixedPoint(0){
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(Fixed &copie){
+Fixed::Fixed(Fixed const &copie){
     std::cout << "Copy constructor called" << std::endl;
     this->_nbFixedPoint = copie.getRawBits();
+}
+
+Fixed::Fixed(const int i):_nbFixedPoint(i << Fixed::_nbBitsFrac){
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float f): _nbFixedPoint(roundf(f * ((float)(1<< Fixed::_nbBitsFrac)))){
+    std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::~Fixed(void){
@@ -27,16 +36,32 @@ Fixed::~Fixed(void){
 
 }
 
-Fixed Fixed::operator= (Fixed assignement){
+float Fixed::toFloat(void) const{
+    return ((float)this->_nbFixedPoint/(float)(1 << Fixed::_nbBitsFrac));
+}
+
+int Fixed::toInt(void) const{
+    return (this->_nbFixedPoint >> Fixed::_nbBitsFrac);
+}
+
+Fixed &Fixed::operator= (Fixed &c){
     std::cout << "Copy assignment operator called "
     << "// <-- This line may be missing depending on your implementation"
     << std::endl;
-    return (assignement);
+    if(this != &c)
+        *this = c;
+    return (*this);
+}
+
+std::ostream  &operator<< (std::ostream &out, const Fixed nb){
+    out <<nb.toFloat();
+    return (out);
 }
 
 int Fixed::getRawBits(void) const{
     return(this->_nbFixedPoint);
 }
+
 void Fixed::setRawBits(int const raw){
     this->_nbFixedPoint = raw;
 }
--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -6,12 +6,13 @@
 /*   By: rliu <rliu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 11:55:53 by rliu              #+#    #+#             */
-/*   Updated: 2022/10/10 18:50:54 by rliu             ###   ########.fr       */
+/*   Updated: 2022/10/10 21:01:28 by rliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+#include <iostream>
 
 class Fixed{
     private:
@@ -20,16 +21,18 @@ class Fixed{
         
     public:
         Fixed(void);
-        Fixed(Fixed &copie);
+        Fixed(Fixed const &copie);
         Fixed(int const i);
         Fixed(float const f);
         ~Fixed(void);
 
-        Fixed   operator= (Fixed c);
+        Fixed   &operator= (Fixed &c);
+       
         int     getRawBits(void) const;
         void    setRawBits(int const raw);
         float   toFloat(void) const;
         int     toInt(void) const;
 };
+ std::ostream  &operator<< (std::ostream &out, Fixed nb);
 
 #endif
